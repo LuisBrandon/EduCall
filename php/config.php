02b60d3617b15d8f69e827aba21b485787df4354
Luis Brandon <luisbrandonbarrones@gmail.com>
2019-06-05 13:49:27 +0200

    Add files via upload
    Cambiados los estilos.
    Añadir después dentro de la carpeta "css" el fontawesome (que no me deja subir tantas cosas)

--- php/config.php
+++ php/config.php
@@ -1,5 +1,17 @@
 <?php
 
 
+/* Database connection start */
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "pruebaproyecto";
+$conn = mysqli_connect($servername, $username, $password, $dbname) or die("Algo ha ido mal: " . mysqli_connect_error());
+
+if (mysqli_connect_errno()) {
+printf("Conexión fallida: %s\n", mysqli_connect_error());
+exit();
+}
+
 
 ?>
